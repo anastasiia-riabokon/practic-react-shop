b50232cc6bfef765ba5c81ad8bbf3968e230155d
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-06-16 20:03:58 +0300

    feat(app): add animation for close modal

--- src/components/StoreApp/StoreApp.jsx
+++ src/components/StoreApp/StoreApp.jsx
@@ -4,6 +4,7 @@ import {useLocalStorage} from "../../hook/useLocalStorage";
 import List from "./List";
 import Modal from "../Modal/Modal";
 import css from "./StoreApp.module.css";
+import {AnimatePresence} from "framer-motion";
 
 export const StoreApp = () => {
   const [products, setProducts] = useLocalStorage("products", productsData);
@@ -25,13 +26,15 @@ export const StoreApp = () => {
         </button>
       </header>
       <List products={products} />
-      {isOpen && (
-        <Modal onClose={toggleModal}>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates veniam reprehenderit
-          nisi omnis, rerum qui molestias voluptatem, repellat sed laboriosam minus, eligendi
-          dolorem animi ullam accusantium. Qui ad accusamus corporis.
-        </Modal>
-      )}
+      <AnimatePresence>
+        {isOpen && (
+          <Modal onClose={toggleModal}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates veniam
+            reprehenderit nisi omnis, rerum qui molestias voluptatem, repellat sed laboriosam minus,
+            eligendi dolorem animi ullam accusantium. Qui ad accusamus corporis.
+          </Modal>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
